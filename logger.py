974b76f3c1f5445b1a2e62b133ea01c736308340
segurosfly <info@segurosfly.com>
2026-05-23 11:51:39 -0500

    Refactor logger to use CallableLogger wrapper

--- logger.py
+++ logger.py
@@ -8,13 +8,38 @@ os.makedirs(DIR_LOGS, exist_ok=True)
 _log_file = os.path.join(DIR_LOGS, f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
 
 logging.basicConfig(
-      level=logging.INFO,
-      format="[%(asctime)s] %(levelname)s  %(message)s",
-      datefmt="%H:%M:%S",
-      handlers=[
-                logging.StreamHandler(),
-                logging.FileHandler(_log_file, encoding="utf-8"),
-      ]
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s  %(message)s",
+    datefmt="%H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(_log_file, encoding="utf-8"),
+    ]
 )
 
-log = logging.getLogger("spv")
+_logger = logging.getLogger("spv")
+
+
+class _CallableLogger:
+    """Logger wrapper that supports both log('msg') and log.info('msg')."""
+
+    def __call__(self, msg, *args, **kwargs):
+        _logger.info(msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        _logger.info(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        _logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        _logger.error(msg, *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        _logger.debug(msg, *args, **kwargs)
+
+    def exception(self, msg, *args, **kwargs):
+        _logger.exception(msg, *args, **kwargs)
+
+
+log = _CallableLogger()
